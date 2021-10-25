fc46b3975c61c304e7518bbac02a4622365a949e
Student <Student@PC-Praktikum-09>
2021-10-25 13:03:40 +0200

    Model Class updated

--- AnwendungFreizeitbaeder_Vorgabe/src/business/FreizeitBaederModel.java
+++ AnwendungFreizeitbaeder_Vorgabe/src/business/FreizeitBaederModel.java
@@ -1,5 +1,26 @@
 package business;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FreizeitBaederModel {
+
+	private Freizeitbad freizeitbad;
+
+	public Freizeitbad getFreizeitbad() {
+		return freizeitbad;
+	}
+
+	public void setFreizeitbad(Freizeitbad freizeitbad) {
+		this.freizeitbad = freizeitbad;
+	}
+
+	public void SchreibeFreizeitBaederInVsvDatei() throws IOException {
+		BufferedWriter aus = new BufferedWriter(new FileWriter("FreizeitBaeder.csv", true));
+		aus.write(this.getFreizeitbad().gibFreizeitbadZurueck(';'));
+		aus.close();
+
+	}
 
 }
